88a2a6b05522aa4db88c37ad835dead52013043b
george fourkas <giorgosfourkas.98@gmail.com>
2021-04-24 22:59:29 +0300

    Initial Commit

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,28 @@
+<template>
+    <select @change="onChange" v-model="selected" class="form-select mt-1 block w-full border p-3 rounded mt-10">
+        <option selected="selected">Επιλέξτε Χώρα</option>
+        <option v-for="country in countries" :value="country.ID">{{ country.Country }}</option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: "CountrySelect",
+    props: ['countries'],
+    data() {
+        return {
+
+            selected: 0
+        }
+    },
+    emits:['country-selected'],
+    methods: {
+        onChange() {
+            const country = this.countries.find((item) => item.ID === this.selected)
+            this.$emit('country-selected', country)
+        }
+    }
+}
+</script>
+
+
